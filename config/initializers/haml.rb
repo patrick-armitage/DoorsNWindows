6ef8fa031082d7d7cfe1d6fa469743edb3b767b8
Patrick Armitage <armitage.patrick@gmail.com>
2013-11-18 12:49:32 -0800

    Add Secure :markdown filter for Haml

---
+++ config/initializers/haml.rb
@@ -0,0 +1,13 @@
+require 'rdiscount'
+
+module Doorsnwindows
+  module Filters
+    module Markdown
+      include Haml::Filters::Base
+
+      def render(text)
+        ::RDiscount.new(text, :filter_html).to_html
+      end
+    end
+  end
+end
