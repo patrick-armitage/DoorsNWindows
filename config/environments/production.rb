2e5e7fda9961964796c54d92c75f6b87ad44aa85
Patrick Armitage <armitage.patrick@gmail.com>
2014-01-22 13:28:01 -0800

    Round 10

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,7 @@ Doorsnwindows::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+  config.assets.precompile += ['landing_pages/backstretch-init.js']
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
