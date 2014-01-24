afc66a5e062d6682a4407f6f8b98586fa70ebd5d
Patrick Armitage <armitage.patrick@gmail.com>
2014-01-24 15:25:59 -0800

    Landing Page Facelift v1

--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -2,25 +2,25 @@ class LandingPagesController < ApplicationController
   layout 'landing_page'
 
   def doors
-    @lead = Lead.new
+    @landinglead = Lead.new
   end
 
   def windows
-    @lead = Lead.new
+    @landinglead = Lead.new
   end
 
   def create
-    @lead = Lead.new(lead_params)
-    if @lead.save
-      MailerWorker.perform_async(@lead.id)
-      @lead.create_activity :create, owner: signed_in? ? current_user : nil, params: @lead.attributes
-      render :thank_you
+    @landinglead = Lead.new(lead_params)
+    if @landinglead.save
+      MailerWorker.perform_async(@landinglead.id)
+      @landinglead.create_activity :create, owner: signed_in? ? current_user : nil, params: @landinglead.attributes
+      render :doors
     end
   end
 
   private
     def lead_params
-      params.require(:lead).permit(:name, :phone, :email, :zip,
+      params.require(:landinglead).permit(:name, :phone, :email, :zip,
                                    :interest, :status, :notes)
     end
 end
