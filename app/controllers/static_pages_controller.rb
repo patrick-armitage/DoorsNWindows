e6a27fffb070d27827fbe4b202a145ecbc11ad9f
Patrick Armitage <patrick@wipomo.com>
2014-02-11 09:20:12 -0800

    Add correct sitemap to build out content.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,10 +3,60 @@ class StaticPagesController < ApplicationController
     @lead = Lead.new
   end
 
-  def about
+  def about_story
+    render 'static_pages/about_us/about'
   end
 
-  def contact
+  def about_services
+    render 'static_pages/about_us/services'
+  end
+
+  def about_guarantee
+    render 'static_pages/about_us/guarantee'
+  end
+
+  def products_windows
+    render 'static_pages/products/windows'
+  end
+
+  def products_doors
+    render 'static_pages/products/doors'
+  end
+
+  def products_hardware
+    render 'static_pages/products/hardware'
+  end
+
+  def mfg_choose
+    render 'static_pages/mfgs/choose'
+  end
+
+  def info_how
+    render 'static_pages/info/how'
+  end
+
+  def info_faqs
+    render 'static_pages/info/faqs'
+  end
+
+  def info_installs
+    render 'static_pages/info/installs'
+  end
+
+  def info_mfgs
+    render 'static_pages/info/mfgs'
+  end
+
+  def info_historic
+    render 'static_pages/info/historic'
+  end
+
+  def contact_email
     @inquiry = Inquiry.new
+    render 'static_pages/contact/email_us'
+  end
+
+  def contact_info
+    render 'static_pages/contact/info'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,33 @@ Doorsnwindows::Application.routes.draw do
 
   resources :leads
   resources :landing_leads
-	resources :users
+  resources :users
   resources :activities
-	resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
   resources :inquiries, only: [:new, :create]
 
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
 
-	match '/about',   to: 'static_pages#about',   via: 'get'
-  match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/about_us/story',      to: 'static_pages#about_story',    via: 'get'
+  match '/about_us/services',   to: 'static_pages#about_services', via: 'get'
+  match '/about_us/guarantee',  to: 'static_pages#about_guarantee',via: 'get'
+  match '/products/windows',    to: 'static_pages#products_windows',via: 'get'
+  match '/products/doors',      to: 'static_pages#products_doors', via: 'get'
+  match '/products/hardware',   to: 'static_pages#products_hardware',via: 'get'
+  match '/manufacturers/choosing',to: 'static_pages#mfg_choose',   via: 'get'
+  match '/info/how',            to: 'static_pages#info_how',       via: 'get'
+  match '/info/faqs',           to: 'static_pages#info_faqs',      via: 'get'
+  match '/info/installs',       to: 'static_pages#info_installs',  via: 'get'
+  match '/info/mfgs',           to: 'static_pages#info_mfgs',      via: 'get'
+  match '/info/historic',       to: 'static_pages#info_historic',  via: 'get'
 
-  match '/savings/doors',   to: 'landing_leads#doors',    via: 'get'
-  match '/savings/windows', to: 'landing_leads#windows',  via: 'get'
+  match '/contact/email',       to: 'static_pages#contact_email',  via: 'get'
+  match '/contact/info',        to: 'static_pages#contact_info',   via: 'get'
+
+  match '/savings/doors',       to: 'landing_leads#doors',    via: 'get'
+  match '/savings/windows',     to: 'landing_leads#windows',  via: 'get'
 
   root to: 'static_pages#home'
 
