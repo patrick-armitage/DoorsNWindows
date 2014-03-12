61020895ba06a61c3f5b57522c8eff0af753e8a7
Patrick Armitage <armitage.patrick@gmail.com>
2014-03-12 11:11:23 -0700

    Revert "Conditional deploy:assets:precompilation"
    This reverts commit f8fc1d1bdf94b0bba953d09cccd9189954194207.

--- config/deploy.rb
+++ config/deploy.rb
@@ -80,13 +80,13 @@ namespace :deploy do
     end
 
     task :precompilation do
-      # run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-      from = source.next_revision(current_revision)
-      if releases.length <= 1 || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-      else
-        logger.info "Skipping deploy:assets:precompilation because there were no asset changes"
-      end
+      run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      # from = source.next_revision(current_revision)
+      # if releases.length <= 1 || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+      #   run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      # else
+      #   logger.info "Skipping asset pre-compilation because there were no asset changes"
+      # end
     end
   end
 end
