5c6be53795861e227b2226235ea3e539a624b2f4
Patrick Armitage <armitage.patrick@gmail.com>
2014-03-11 17:26:19 -0700

    Alter Cap assets:precompile task

--- config/deploy.rb
+++ config/deploy.rb
@@ -78,6 +78,16 @@ namespace :deploy do
         task t.name, t.options, &t.body
       end
     end
+
+    task :precompile, :roles => :app, :except => { :no_release => true } do
+      run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      # from = source.next_revision(current_revision)
+      # if releases.length <= 1 || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+      #   run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      # else
+      #   logger.info "Skipping asset pre-compilation because there were no asset changes"
+      # end
+    end
   end
 end
 
